a85069f749834a2bcbdabcf25ea6288b7a6369d3
Jan Rommel <1jan2rom@gmail.com>
2024-09-09 15:21:09 +0200

    Urlshortener fertig

--- index_alt.js
+++ index_alt.js
@@ -9,35 +9,7 @@ const app = express();
 // Basic Configuration
 const port = process.env.PORT || 3000;
 let urls_obj = {};
-/*
-//Laden
-app.use(() => {
-fs.readFile('urls.json', 'utf8', 
-  (err, data) => {
-     if (err) {
-       console.error(err);
-       return;
-  }
-  urls_obj = JSON.parse(data);
-  console.log("erg:", urls_obj);
-  });
-  
-}); 
-  for(let j = 0; j < urls_obj.length; j++){
-	  if (urls_obj[j].url == "www.youtube.com"){
-		  console.log(urls_obj[j].id);
-	  }
-  }
-  urls_obj[urls_obj.length] = { url: "www.der_bieber_bert.de", id: 665 };
-	console.log(urls_obj);
-	
-	//Speichern
-	fs.writeFile('urls.json', JSON.stringify(urls_obj), function (err) {
-  if (err) throw err;
-  console.log('Saved!');
-}); 
-  
-});*/
+/**/
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -53,11 +25,10 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
-	//let url_arr = [["www.google.com", 1],["freeCodeCamp.org", 1156, 2],["forum.freecodecamp.org", 231, 3], ["mail.google.com", 1092, 4], ["mail.ru", 6969, 5], ["www.mail.com", 6],["www.youtube.com", 90],["www.brandbucket.com", 10011]];
-
-// Your first API endpoint
+	
+// GET
 app.get('/api/shorturl/:url1?', function(req, res) {
-	console.log("get", req.params.url1);
+	//console.log("get", req.params.url1);
 	//console.log(req.params.url1);
 	//json laden 
 	let url_num = parseInt(req.params.url1);
@@ -73,67 +44,20 @@ app.get('/api/shorturl/:url1?', function(req, res) {
 	let j = 0;
 	for(j = 0; j < urls_obj.length; j++){
 	  if (urls_obj[j].short_url == url_num){
-		  console.log("gefunden:", urls_obj[j].short_url);
+		  //console.log("gefunden:", urls_obj[j].short_url);
 		  return res.redirect(urls_obj[j].original_url);
 		  break;
 	  }
   }
   if ( j >= urls_obj.length || url_num == undefined || isNaN( url_num  ) ){
-		console.log("get invalid url");
+		//console.log("get invalid url");
 		return res.json({ error : 'invalid url' });
   }
-  /*
-	let error_var = true;
-	let num;
-	if(!(isNaN(url_num)) && url_num < urls_obj.length && url_num > 0){ 
-		for(let j = 0; j < urls_obj.length; j++){
-			if (urls_obj[j].id == url_num){
-			console.log(urls_obj[j].id);
-			num = j;
-		}
-	}*/
+  /**/
 			console.log("Route valid url");
-	//console.log(url_arr[url_num-1], typeof url_arr[url_num-1]);
-	//console.log("url:", (req));
 	/*
-	const options = { 
-		all:true, 
-		}; 
-	//options.all = true;
-	dns.lookup(urls_obj[num].url, options , (err, address) => {  
-        console.log('address: %j', address);  
-		if (err) {
-                console.log("dns error:", err);
-                return res.json({ error: 'invalid url', message: 'DNS lookup failed' });
-		}
-		if(address !== undefined && Array.isArray(address) && address.length > 0 && address != "undefined"){
-			console.log('DNS addresses found:', address);
-                res.redirect("https://" + urls_obj[num].url);
-                console.log("Route gut");
-		} else {
-			//
-			//console.log(parseInt(req.params.url1));
-	//res.json({ error : 'invalid url' }); 
-			console.log("No valid addresses found");
-                res.json({ error: 'invalid url', message: 'No addresses found' });
-		}
-		
-	});
-	} else {
-	//if (error_var) {
-	res.json({ error : 'invalid url', url : req.params.url1 });
-	}
-	
-	});*/
-	/*if (parseInt(req.params.url) > 0){
-	
-	let url_arr = ["https://www.google.com/","https://freeCodeCamp.org","https://forum.freecodecamp.org/", "https://mail.google.com/mail/u/0/", "https://mail.ru/", "https://www.mail.com/consentpage"];
-	let url_num = parseInt(req.params.url);
-  res.json({ original_url: url_arr[url_num -1], short_url : url_num });
-	} else {
-	
-		res.json({ error : 'invalid url' });
-	}*/
+	*/
+	/**/
 	});
 });
 
@@ -145,7 +69,7 @@ app.post('/api/shorturl', function(req, res) {
 	//Url testen, muss mit http anfangen
 	if (req.body.url.substring(0,7) != "http://" && req.body.url.substring(0,8) != "https://"){
 		
-		  console.log("Invalid");
+		  //console.log("Invalid");
 		return res.json({ error : 'Invalid URL', url: req.body.url })
 	}
 	//url vorbereiten
@@ -169,8 +93,8 @@ app.post('/api/shorturl', function(req, res) {
 	let j = 0;
 	for(j = 0; j < urls_obj.length; j++){
 	  if (urls_obj[j].original_url == search){
-		  console.log("gefunden j:", urls_obj[j].original_url);
-		  console.log("return j:", urls_obj[j].original_url,  urls_obj[j].short_url);
+		  //console.log("gefunden j:", urls_obj[j].original_url);
+		  //console.log("return j:", urls_obj[j].original_url,  urls_obj[j].short_url);
 		  return res.json({ original_url : urls_obj[j].original_url, short_url: urls_obj[j].short_url });
 		  break;
 	  }
@@ -182,9 +106,9 @@ app.post('/api/shorturl', function(req, res) {
 		if (search.substring(0,21) == "http://localhost:3000"){
 			//return res.json({ error : 'Invalid URL', url: req.body.url })
 			let t = 0;
-			for(t = 0; t < urls_obj.length; t++){
-			if (urls_obj[t].original_url == search.substring(0,21)){
-				console.log("return j:", urls_obj[t].original_url,  urls_obj[t].short_url);
+			for(t = 0; t < urls_obj.length; t++){//.substring(0,21)
+			if (urls_obj[t].original_url == search){
+				//console.log("return j:", urls_obj[t].original_url,  urls_obj[t].short_url);
 				return res.json({ original_url : urls_obj[t].original_url, short_url: urls_obj[t].short_url });
 				break;
 			}
@@ -198,20 +122,20 @@ app.post('/api/shorturl', function(req, res) {
 			for(t = 0; t < urls_obj.length; t++){
 				if (urls_obj[t].short_url == new_url_eig){
 					//wenn die short_url schon vergeben ist, dann rechne +1 auf und such nochmal von Vorne
-					console.log("gefunden:", urls_obj[t].short_url);
+					//console.log("gefunden:", urls_obj[t].short_url);
 					new_url_eig++;
 					t = 0;
 				}
 			}
-			//short_url ist nicht enthalten, also einfügen und zurückgeben
-			urls_obj[urls_obj.length] = { original_url : search.substring(0,21), short_url: new_url_eig };
+			//short_url ist nicht enthalten, also einfügen und zurückgeben .substring(0,21)
+			urls_obj[urls_obj.length] = { original_url : search, short_url: new_url_eig };
 			//Speichern
 			fs.writeFile('urls.json', JSON.stringify(urls_obj), function (err) {
 			if (err) throw err;
 				console.log('Saved!');
 			}); 
-			 console.log("return j:", urls_obj);
-			return res.json({ original_url : search.substring(0,21), short_url: new_url_eig });	
+			 //console.log("return j:", urls_obj); //.substring(0,21)
+			return res.json({ original_url : search, short_url: new_url_eig });	
 		}
 		//short url erzeugen
 		//ist die url legitim? --> DNS Probe
@@ -242,7 +166,7 @@ app.post('/api/shorturl', function(req, res) {
 			for(k = 0; k < urls_obj.length; k++){
 				if (urls_obj[k].short_url == new_url){
 					//wenn die short_url schon vergeben ist, dann rechne +1 auf und such nochmal von Vorne
-					console.log("gefunden:", urls_obj[k].short_url);
+					//console.log("gefunden:", urls_obj[k].short_url);
 					new_url++;
 					k = 0;
 				}
@@ -254,7 +178,7 @@ app.post('/api/shorturl', function(req, res) {
 			if (err) throw err;
 				console.log('Saved!');
 			}); 
-			 console.log("return j:", urls_obj);
+			 //console.log("return j:", urls_obj);
 			return res.json({ original_url : search, short_url: new_url });
 		
 			}
@@ -266,48 +190,7 @@ app.post('/api/shorturl', function(req, res) {
 	
 	//console.log("req:", req);
 	//console.log(req.body);
-	/*if (req.body.url.substring(0,7) != "http://" && req.body.url.substring(0,8) != "https://" && req.body.url.substring(0,3) != "www"){
-		res.json({ error : 'invalid url', url: req.body.url });	
-	}
-	
-	let i;
-	const options = { 
-		all:true, 
-		}; 
-	let url_arr = [["www.google.com", 1],["freeCodeCamp.org", 1156],["forum.freecodecamp.org", 231],["www.example.com", 1336], ["mail.google.com", 1092], ["mail.ru", 6969], ["www.mail.com", 6],["www.youtube.com", 90]];
-	let search = req.body.url;
-	if(req.body.url.substring(0,8) == "https://"){
-		search = req.body.url.substring(8); 
-	}
-	console.log("search", search);
-	let url_length = url_arr.length;
-	dns.lookup(search, options , (err, address) => {  
-        console.log('address: %j', address);  
-		if (err) {
-                console.log("dns error:", err);
-                return res.json({ error: 'invalid url', message: 'DNS lookup failed' });
-		}
-	if(req.body.url != "" && req.body.url != undefined ){
-
-	for (i = 0; i < url_length; i++){
-		console.log("Das sollte irg wann wahr werden",url_arr[i][0] == search, url_arr[i], search);
-		if(url_arr[i][0] == search){
-			return res.json({ original_url : "https://"+search, short_url : url_arr[i][1]});
-			break
-		}
-		
-	}
-	}
-	});
-	if(i >= url_length){
-	//console.log(req.body.url);
-	res.json({ error : 'invalid url', url: req.body.url });
-	//let url_num = parseInt(req.params.url1);
-	
-	} else {
-		console.log("Wie bist du hier her gekommen?", search, i);
-		//res.json({ error : 'invalid url', url: req.body.url });
-	}*/
+	/**/
 });
 
 app.listen(port, function() {
